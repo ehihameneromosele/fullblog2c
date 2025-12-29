7dc16cfae3d620d46eff464f50a2fbe8fafca012
ehihameneromosele <ehihameneromosele@gmail.com>
2025-12-29 00:55:26 +0000

    installing dependencies

--- blogbackc/build.sh
+++ blogbackc/build.sh
@@ -1,8 +1,7 @@
 set -o errexit 
+pip install --upgrade pip
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 
-
-python manage.py makemigrations
 python manage.py migrate
